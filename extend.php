f5a0c38d10faa6be89476f80458ff48cecb6f719
Mark Cutting <2557946+phenomlab@users.noreply.github.com>
2021-02-01 15:52:46 +0000

    Update extend.php

--- extend.php
+++ extend.php
@@ -14,14 +14,17 @@ use Flarum\Frontend\Document;
 return [
     (new Extend\Frontend('forum'))
         ->content(function (Document $document) {
-			$document->head[] = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.css">';
-			$document->head[] = '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>';
-$document->head[] = '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.js"></script>';
- $document->foot[] = <<<HTML
+            $document->head[] = '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>';
+            $document->head[] = '<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>';
+            $document->head[] = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css">';
+            $document->foot[] = <<<HTML
 <script>
   flarum.core.compat.extend.extend(flarum.core.compat['components/CommentPost'].prototype, 'oncreate', function(output, vnode) {
-$("img").not('.emoji').attr("data-fancybox", "");
-  });
+$("img").wrap(function() {
+ return "<a data-fancybox='gallery' href='" + this.src + "'/>";
+ $('img').removeAttr('data-fancybox');
+});
+});
 </script>
 HTML;
         })
